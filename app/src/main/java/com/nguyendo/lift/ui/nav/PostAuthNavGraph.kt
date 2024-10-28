b4fca6743f6c515dd89f1848713f1e69533fa220
Nguyen Do <nguyldo@gmail.com>
2024-10-28 16:57:47 -0700

    auth (#4)

--- app/src/main/java/com/nguyendo/lift/ui/nav/PostAuthNavGraph.kt
+++ app/src/main/java/com/nguyendo/lift/ui/nav/PostAuthNavGraph.kt
@@ -1,9 +1,15 @@
 package com.nguyendo.lift.ui.nav
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.compose.composable
@@ -13,23 +19,24 @@ import com.nguyendo.lift.ui.views.FeedView
 import com.nguyendo.lift.ui.views.ProfileView
 
 @Composable
-fun PostAuthNavHost(logout: () -> Unit) {
+fun PostAuthNavGraph(logout: () -> Unit) {
     val navController = rememberNavController()
     Scaffold(
         bottomBar = {
             TabBar(navController)
         },
     ) { padding ->
-        NavHost(
-            navController = navController,
-            startDestination = Feed,
-            modifier = Modifier.padding(padding),
-        ) {
-            composable<Feed> {
-                FeedView()
-            }
-            composable<Profile> {
-                ProfileView(logout)
+        Column(modifier = Modifier.padding(padding)) {
+            NavHost(
+                navController = navController,
+                startDestination = Feed
+            ) {
+                composable<Feed> {
+                    FeedView()
+                }
+                composable<Profile> {
+                    ProfileView(logout)
+                }
             }
         }
     }
